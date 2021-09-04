9f22e8f0bd9d5e8e6c02f26c4db0acfc7d2459a4
Trevor Wiegand <trevorwiegand92@gmail.com>
2021-09-03 22:48:04 -0500

    updated spelling error

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
